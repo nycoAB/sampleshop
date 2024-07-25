9a2a09b59401dc7651ed8a26b0b994667413406b
AbolFazl <abolfazl.akhondi96@gmail.com>
2024-07-25 08:17:50 +0330

    admin controllers added

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,47 +1,26 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using MyShopApi.App.DTOs.ProductDTOs;
 using MyShopApi.Models;
 using MyShopApi.Repositories;
 
 namespace MyShopApi.Controllers
 {
-    [Route("/api/category")]
+    [Route("api/category")]
     [ApiController]
-    public class CategoryController : Controller
+    public class CategoryController : ControllerBase
     {
         private readonly ICategoryRepo _repo;
-        private readonly IMapper _mapper;
-        public CategoryController(ICategoryRepo repo , IMapper mapper)
+        public CategoryController(ICategoryRepo repo, IMapper mapper)
         {
             _repo = repo;
-            _mapper = mapper;
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<Category>> GetAll() { 
+        public ActionResult<IEnumerable<Category>> GetAll()
+        {
             var categories = _repo.GetCategories();
             return Ok(categories);
         }
-
-        [HttpPost]
-        public IActionResult Create(CategoryInput categoryinput)
-        {   
-            var category = _mapper.Map<Category>(categoryinput);
-            _repo.CreateCategory(category);
-            return Ok();
-        }
-        [HttpPut("{id}")]
-        public IActionResult Update(int id, CategoryInput categoryinput)
-        {   
-            var category = _mapper.Map<Category>(categoryinput);
-            _repo.UpdateCategory(id, category);
-            return Ok();
-        }
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id) { 
-            _repo?.DeleteCategory(id); 
-            return Ok();
-        }
     }
-}   
+}
---
+++ Controllers/_Admin/CategoryController.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyShopApi.App.DTOs.ProductDTOs;
+using MyShopApi.Models;
+using MyShopApi.Repositories;
+
+namespace MyShopApi.Controllers._Admin
+{
+    [Route("/api/admin/category")]
+    [ApiController]
+    [Authorize]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepo _repo;
+        private readonly IMapper _mapper;
+        public CategoryController(ICategoryRepo repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult Create(CategoryInput categoryinput)
+        {
+            var category = _mapper.Map<Category>(categoryinput);
+            _repo.CreateCategory(category);
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, CategoryInput categoryinput)
+        {
+            var category = _mapper.Map<Category>(categoryinput);
+            _repo.UpdateCategory(id, category);
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _repo?.DeleteCategory(id);
+            return Ok();
+        }
+    }
+}
